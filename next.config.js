cea426b47804999168551bfdd955987fd0bf6cbb
Cristian-DW <cristtiiank@gmail.com>
2026-05-18 18:13:14 -0500

    feat: initialize approuter configuration and project infrastructure with MTA deployment support

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: 'standalone',
     serverComponentsExternalPackages: ['pg', 'bcrypt'],
     experimental: {
         serverComponentsExternalPackages: ['pg', 'bcrypt'],
